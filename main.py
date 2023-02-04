75515cd8af6a89f1492416f993c89bb97b5b22f1
endeavor <220100kd@yse-c.net>
2023-02-04 10:36:25 +0900

    プレイヤーの移動処理

--- main.py
+++ main.py
@@ -14,10 +14,46 @@ player_img = tkinter.PhotoImage(file="img/bird.png")  # プレイヤー画像の
 bg_posy = 0  # 背景の中心のy座標
 px, py = 240, 540  # プレイヤーの位置座標（値は初期位置）
 
+# ===========  キー操作  ==============
+key = ""  # 押されたキーの値
+key_off = True  # キーが押されてないフラグ
+
+# キーが押された時
+def key_down(e):
+    # グローバル変数
+    global key, key_off
+    # keyの値を押されたキーにする
+    key = e.keysym
+    # キーが押されてないフラグをFalse
+    key_off = False
+
+
+# キーが離された時
+def key_up(e):
+    # グローバル変数
+    global key_off
+    # キーが押されてないフラグをTrue
+    key_off = True
+
+
+# =================================
+
 # プレイヤーの移動処理
 def move_player():
     # グローバル変数
     global px, py
+    # 左キーを押す and pxが30より大きい時、pxの値を-10
+    if key == "Left" and px > 30:
+        px -= 10
+    # 右キーを押す and pxが450より小さい時、pxの値を＋10
+    if key == "Right" and px < 450:
+        px += 10
+    # 上キーを押す and pyが40より大きい時、pyの値を-10
+    if key == "Up" and py > 40:
+        py -= 10
+    # 下キーを押す and pyが600より小さい時、pyの値を+10
+    if key == "Down" and py < 610:
+        py += 10
     # プレイヤーの描画
     canvas.create_image(px, py, image=player_img, tag="SCREEN")
 
@@ -25,7 +61,7 @@ def move_player():
 # メイン処理
 def main():
     # グローバル変数
-    global bg_posy
+    global bg_posy, key, key_off
     bg_posy = (bg_posy + 1) % 640  # 背景の中心のy座標(0~639)
     canvas.delete("SCREEN")  # キャンバス上の全てを削除
     # 背景の描画（中心のx座標, 中心のy座標, 画像, タグ)
@@ -33,9 +69,15 @@ def main():
     canvas.create_image(240, bg_posy + 320, image=bg_img, tag="SCREEN")
     # プレイヤー処理
     move_player()
+    # キーが押されていない時、キーの操作を初期値にする
+    if key_off == True:
+        key = ""
+        key_off = False
     # 50m秒後にmainを再び実行
     window.after(50, main)
 
 
+window.bind("<KeyPress>", key_down)  # キーを押した時にkey_down関数を呼び出す
+window.bind("<KeyRelease>", key_up)  # キーを離した時にkey_up関数を呼び出す
 main()  # メイン関数
 window.mainloop()  # ウィンドウの表示
